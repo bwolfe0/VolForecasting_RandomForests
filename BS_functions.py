3dfc2a7360d25dc34e81100cd9e5134479650a1e
Ben Wolfe <benjaminwolfe@ucsb.edu>
2023-12-19 14:27:49 -0500

    Added Put functionality

--- BS_functions.py
+++ BS_functions.py
@@ -8,13 +8,14 @@ def is_numeric(value):
     return isinstance(value, (int, float, complex)) and not isinstance(value, bool)
 
 
-def GetCall(S,T,r,K,sigma):
+def GetPremium(S,T,r,K,sigma,flag='c'):
     '''Find the Black Scholes price of a European Call Option
         S: Stock Price at time 0
         T: Time to expiration (annualized)
         r: Annual risk free rate
         K: Strike Price
         sigma: Annualized Volatility
+        flag: option type, 'c' or 'p'
     '''
     for i, val in enumerate([S,T,r,K,sigma]):
         if is_numeric(val) is False: raise TypeError(f"Inputs should be numeric: {val}")
@@ -22,23 +23,31 @@ def GetCall(S,T,r,K,sigma):
 
     d1 = (log(S/K) + (r + sigma**2/2)*T)/(sigma*sqrt(T))
     d2 = d1 - sigma * sqrt(T)
-    return S*N(d1) - K*exp(-r*T)*N(d2)
+
+    if flag == 'c':
+        return S*N(d1) - K*exp(-r*T)*N(d2)
+    else:
+        return -S*N(-d1) + K*exp(-r*T)*N(-d2)
 
 
-def GetVega(S,T,r,K,sigma):
+def GetVega(S,T,r,K,sigma,flag='c'):
     '''Find the "Vega" of a Black Scholes European Call Option
     S: Stock Price at time 0
     T: Time to expiration (annualized)
     r: Annual risk free rate
     K: Strike Price
     sigma: Annualized Volatility
+    flag: option type, 'c' or 'p'
     '''
     for i, val in enumerate([S,T,r,K,sigma]):
         if is_numeric(val) is False: raise TypeError(f"Inputs should be numeric: {val}")
         if val <= 0: raise ValueError(f"Inputs should be greater than zero: {['S','T','r','K','sigma'][i]} = {val}")
-
+    
     d1 = (log(S/K) + (r + sigma**2/2)*T)/(sigma*sqrt(T))
-    return S*sqrt(T)*n(d1)
+    if flag == 'c':
+        return S*sqrt(T)*n(d1)
+    else:
+        return -S*sqrt(T)*n(-d1)
 
 
 def GetIV(target_value,S,T,r,K,sigma_guess=.5):
@@ -57,20 +66,20 @@ def GetIV(target_value,S,T,r,K,sigma_guess=.5):
 
     sigma = sigma_guess
     max_iterations = 200
-    precision = 1e-8
+    precision = 1e-5
 
     for i in range(max_iterations):
-        price = GetCall(S,T,r,K,sigma)
+        price = GetPremium(S,T,r,K,sigma)
         vega = GetVega(S,T,r,K,sigma)
 
         diff = target_value - price
 
         if abs(diff) < precision:
-            return {'Sigma': sigma, 'Number of Iterations': i}
+            return {'IV': sigma, 'Number of Iterations': i}
         
         #Use Newton Method to find next step. Formula is x_n = x_{n-1} - f(sigma)/f'(sigma). Here, f(sigma) = diff
         #Plus sign comes from the definition of diff. Derivitive of diff WRT sigma causes double negative
-        #Times 100 factor helps with stable convergence
-        sigma = abs(sigma + diff/(100*vega))
+        #Times 10 factor helps with stable convergence
+        sigma = abs(sigma + diff/(10*vega))
 
-    return sigma
+    return {'IV': sigma, 'Iterations': max_iterations}
